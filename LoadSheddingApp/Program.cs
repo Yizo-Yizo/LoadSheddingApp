3d722ea1b71aeeebb12b5a811696e6a9d8362e53
Thapelo Selebedi <selebedi.thapelo6@gmail.com>
2021-07-28 21:31:53 +0200

    Done

--- LoadSheddingApp/Program.cs
+++ LoadSheddingApp/Program.cs
@@ -6,25 +6,40 @@ namespace LoadSheddingApp
     {
         public static void Main(string[] args)
         {
+            int action = 0;
             DisplayInfo info = new DisplayInfo();
 
-            Console.Write("Enter 1 if you want to check loadshedding schedule or -1 if you want to exit: ");
-            int action = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.Write("Enter 1 if you want to check loadshedding schedule or -1 if you want to exit: ");
+                action = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("You have entered invalid key. Please enter a valid key!");
+            }
 
             while (action == 1)
             {
-                Console.Write("\nEnter a number of your surburb: ");
-                int number = Convert.ToInt32(Console.ReadLine());
+                try
+                {
+                    Console.Write("\nEnter a number of your surburb: ");
+                    int number = Convert.ToInt32(Console.ReadLine());
 
-                info.LoadSheddingDuration(number);
-                info.Result();
+                    info.LoadSheddingDuration(number);
+                    info.Result();
 
-                Console.Write("\nEnter 1 if you want to check loadshedding schedule or -1 if you want to exit: ");
-                action = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("\nEnter 1 if you want to check loadshedding schedule or -1 if you want to exit: ");
+                    action = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("You have entered invalid key. Please enter a valid key!");
+                }
             }
 
             Console.WriteLine("\nHave a nice day!");
-            
+
         }
     }
 }
